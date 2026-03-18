47bc54b40dee73b528fbfba94fbe1a45b5e2d754
Anton Sakhanovych <dev@antonsakhanovych.com>
2026-03-18 14:20:27 -0400

    Create Github OIDC Provider with correct IAM roles

--- main.tf
+++ main.tf
@@ -7,3 +7,24 @@ locals {
     ManagedBy = "OpenTofu"
   }
 }
+
+module "github_oidc" {
+  source = "./modules/github-oidc"
+
+  infrastructure_repositories = [
+    "ZeroVerify/infrastructure",
+  ]
+
+  lambda_deployment_repositories = [
+    "ZeroVerify/bitstring-updater-lambda",
+    "ZeroVerify/free-lambda",
+    "ZeroVerify/revocation-lambda",
+    "ZeroVerify/issuer-lambda",
+  ]
+
+  infrastructure_role_name    = "ZeroVerifyGitHubActionsInfra"
+  lambda_deployment_role_name = "ZeroVerifyGitHubActionsLambdaDeployment"
+
+  aws_region = local.aws_region
+  tags       = local.common_tags
+}
