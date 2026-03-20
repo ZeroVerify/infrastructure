9eda7ca00ed8fb46d8fe64af1ceab47958c7768f
Anton Sakhanovych <dev@antonsakhanovych.com>
2026-03-19 21:19:17 -0400

    Give ZeroVerify/circuits repository access to S3 Artifacts bucket

--- modules/github-oidc/main.tf
+++ modules/github-oidc/main.tf
@@ -117,3 +117,65 @@ resource "aws_iam_role_policy_attachment" "lambda_deployment_policy" {
   role       = aws_iam_role.lambda_deployment.name
   policy_arn = aws_iam_policy.lambda_deployment.arn
 }
+
+resource "aws_iam_policy" "artifact_upload" {
+  name        = "GitHubActionsArtifactUpload"
+  description = "Allow GitHub Actions to upload circuits and other artifacts to S3"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "S3ArtifactUpload"
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:PutObjectAcl",
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          var.artifacts_bucket_arn,
+          "${var.artifacts_bucket_arn}/*"
+        ]
+      }
+    ]
+  })
+
+  tags = var.tags
+}
+
+resource "aws_iam_role" "artifact_upload" {
+  name        = var.artifact_deployment_role_name
+  description = "Role for artifact uploads via GitHub Actions"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = [
+              for repo in var.artifact_repositories : "repo:${repo}:*"
+            ]
+          }
+        }
+      }
+    ]
+  })
+
+  tags = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "artifact_upload_policy" {
+  role       = aws_iam_role.artifact_upload.name
+  policy_arn = aws_iam_policy.artifact_upload.arn
+}
