e4a1ac5b2bb21b154092ad94b2b9ba17883fd4b8
Anton Sakhanovych <dev@antonsakhanovych.com>
2026-03-26 01:22:23 -0400

    Add keycloak domain validation records

--- modules/dns/main.tf
+++ modules/dns/main.tf
@@ -11,6 +11,23 @@ resource "cloudflare_dns_record" "www" {
   proxied = false
 }
 
+resource "cloudflare_dns_record" "keycloak" {
+  zone_id = data.cloudflare_zone.main.id
+  name    = "keycloak"
+  content = "cnzozeob.up.railway.app"
+  type    = "CNAME"
+  ttl     = 300
+  proxied = false
+}
+
+resource "cloudflare_dns_record" "keycloak_railway_verify" {
+  zone_id = data.cloudflare_zone.main.id
+  name    = "_railway-verify.keycloak"
+  content = "railway-verify=321ff74d93180e8d2fbd9556d67ae3b23c951fa40e25905f11676e13c580df8e"
+  type    = "TXT"
+  ttl     = 300
+}
+
 resource "cloudflare_dns_record" "github_pages_challenge" {
   zone_id = data.cloudflare_zone.main.id
   name    = "_github-pages-challenge-zeroverify"
@@ -49,4 +66,3 @@ resource "aws_route53_record" "api_latency" {
     region = each.key
   }
 }
-
